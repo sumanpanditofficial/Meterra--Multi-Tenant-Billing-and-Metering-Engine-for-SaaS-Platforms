2bd92a141ada3522911bdd419476f5fd62e35336
Suman Pandit <sumanpanditofficial@gmail.com>
2026-01-25 09:52:13 +0545

    Understood the mapping of entity and created the entity class for phase4

--- src/main/java/com/billing_engine/BillingApplication/model/entity/Invoice.java
+++ src/main/java/com/billing_engine/BillingApplication/model/entity/Invoice.java
@@ -1,11 +1,50 @@
 package com.billing_engine.BillingApplication.model.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
 import lombok.Data;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @Entity
 @Table(name = "invoices")
 public class Invoice {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "tenant_id", nullable = false)
+    private Long tenantId;
+
+    @Column(name = "period_start", nullable = false)
+    private Timestamp periodStart;
+
+    @Column(name = "period_end", nullable = false)
+    private Timestamp periodEnd;
+
+    @Column(name = "subtotal", nullable = false)
+    private BigDecimal subtotal;
+
+    @Column(name = "tax", nullable = false)
+    private BigDecimal tax;
+
+    @Column(name = "total_amount", nullable = false)
+    private BigDecimal total_amount;
+
+    @Column(name = "status", nullable = false)
+    private String status;
+
+    @Column(name = "created_at", nullable = false)
+    private Timestamp createdAt;
+
+
+    @OneToMany(mappedBy = "invoice", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    @JsonBackReference
+    private List<InvoiceItems> items = new ArrayList<>();
+
 }
--- src/main/java/com/billing_engine/BillingApplication/model/entity/InvoiceItems.java
+++ src/main/java/com/billing_engine/BillingApplication/model/entity/InvoiceItems.java
@@ -1,11 +1,34 @@
 package com.billing_engine.BillingApplication.model.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
 import lombok.Data;
+
+import java.math.BigDecimal;
 
 @Data
 @Entity
 @Table(name = "invoice_items")
 public class InvoiceItems {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "invoice_id", nullable = false)
+    @JsonBackReference
+    private Invoice invoice;
+
+    @Column(name = "description",nullable = false)
+    private String description;
+
+    @Column(name = "total_units", nullable = false)
+    private Long totalUnits;
+
+    @Column(name = "unit_price", nullable = false)
+    private BigDecimal unitPrice;
+
+    @Column(name = "amount", nullable = false)
+    private BigDecimal amount;
 }
